40b91560bf75750980f8bf0373b93feb8710ed06
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-10-01 18:08:12 -0300

    ejercicio 3

--- tp1/ejercicio3.py
+++ tp1/ejercicio3.py
@@ -1,82 +1,51 @@
-from util.general import *
-
-class Uniform_distribution:
-	def __init__(self, a, b):
-		if(a > b):
-			raise ValueError('[a,b] must be an interval.')
-		self.a = a
-		self.b = b
-
-	def generate(self,u):
-		x = u*(self.b-self.a)+self.a
-		return x
-
-	def generate_list(self, n):
-		generator = generador.GeneradorLinealCongruente()
-		l = generator.generar_lista(n)
-		for i in range(n):
-			l[i] = self.generate(l[i])
-
-		return l
-	def get_media(self):
-		return (self.b+self.a)/2.0
-	def get_variance(self):
-		return ((self.b+self.a)**2)/12.0
- 
-
-class Normal_standar:
-	def __init__(self):
-		self.media = 0
-		self.desvio= 1
-		self.uniform = Uniform_distribution(0,1.0/math.sqrt(2.0*math.pi))
-
-	def density_function(self, x):
-		return (1.0/(math.sqrt(2.0*math.pi)))*math.exp(-(x**2)/2)
+import util.generador as generador
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-	def generate_list(self, n):
-		l = self.uniform.generate_list(n)
-		g = generador.GeneradorLinealCongruente()
-		for i in range(n):
-			x = self.generate(l[i])
-			u2 = g.generar()
-			if(u2>0.5):
-				l[i] = x
-			else:
-				l[i] = -x
-		return l
+x=[0, 0.00003, 0.00135, 0.00621, 0.02275, 0.06681, 0.11507, 0.15866, 0.21186, 0.27425, 0.34458, 0.42074, 0.5, 0.57926, 0.65542, 0.72575, 0.78814, 0.84134, 0.88493, 0.93319, 0.97725, 0.99379, 0.99865, 0.99997, 1]
+y=[-5, -4, -3, -2.5, -2, -1.5, -1.2, -1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.5, 2, 2.5, 3, 4, 5]
 
-	def generate(self,u):
-		limit = 1.0/math.sqrt(2.0*math.pi)
-		if( u < 0 or u > limit):
-			raise ValueError(' u =%d must be smaller than %f.' %(u, limit))
+# Con estos valores de X e Y obtengo lo que aparenta ser la inversa de una CDF de normal
+# Si intercambio X e Y obtengo la CDF de una normal
+f = interp1d(x, y)
+plt.plot(x, y)
+plt.title("DEBERIA SER LA INVERSA DE LA CDF DE UNA NORMAL")
+plt.show()
 
-		x = math.sqrt(-2.0*math.log(u*math.sqrt(2.0*math.pi)))
-		return x
+class GeneradorNormal:
+  def __init__(self):
+    self.generador = generador.GeneradorLinealCongruente()
+    self.inverse = interp1d(x, y)
+
+  def generar(self):
+    u = self.generador.generar()
+
+    return self.inverse(u)
+
+  def generar_lista(self, cantidad):
+    lista = []
+
+    for i in range(cantidad):
+      lista.append(self.generar())
+
+    return lista
 
 
-	def get_media(self):
-		return self.media
+g = GeneradorNormal()
 
-	def get_moda(self):
-		return self.media
+print("GENERAR HISTOGRAMA")
+lista = g.generar_lista(100000)
+plt.hist(lista)
+plt.title("HISTOGRAMA DE 100000 NUMEROS GENERADOS")
+plt.savefig('./graficos/ejercicio3-histograma.png')
+plt.show()
+print("FIN GENERAR HISTOGRAMA")
+print("\n")
 
-	def get_variance(self):
-		return self.desvio
 
-if __name__ == '__main__':
-	print("######################################################")
-	print("EJERCICIO 3")
-	print("######################################################")
-	print("\n")
-
-	cant = 10000
-	normal = Normal_standar()
-	l = normal.generate_list(cant)
-	generate_histogram(l, "HISTOGRAMA %d NUMEROS GENERADOS" %(cant),'./graficos/ejercicio3-histograma.png');
-
-	print("La media teorica de una V.A uniforme es: %f" %(normal.get_media()))
-	print("La media es: %f" %(get_media(l)) )
-	print("La varianza teorica de una v.A uniforme es: %f" %(normal.get_variance()))
-	print("La varianza experimental es: %f" %(get_variance(l)))
-	print("La moda es: %f" %(get_modas(l)))
+print("La media deberia de una normal estandar debreia ser 0")
+print("La media es: %f" % (np.mean(lista)))
+print("La varianza deberia ser 1")
+print("La varianza es: %f" % (np.var(lista)))
